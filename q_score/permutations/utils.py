0c0b86fe41038ad72e904f0c0255af8489120d7f
vahdeta <suljicv@slu.edu>
2024-11-12 13:24:46 -0600

    Adding in q score scales

--- q_score/permutations/utils.py
+++ q_score/permutations/utils.py
@@ -1,8 +1,7 @@
-import os
+import json
 import logging
-import pydicom
 import requests
-import subprocess
+import numpy as np
 from pathlib import Path
 
 # Set up logging
@@ -25,6 +24,29 @@ def get_series_description(nifti_path: Path):
         return "error"
 
     return task_name
+
+def load_scaling_params(path_to_params: Path):
+    """
+    path_to_params: Path to json file that contains scaling parameters
+    
+    Returns: params dict: A dictionary with keys 'data_min', 'data_max', 'scaled_mean', and 'scaled_std', 
+        e.g., {"data_min": 2.8, "data_max": 11.2, "scaled_mean": 56.39, "scaled_std": 39.03}.
+    """
+
+    with open(path_to_params, 'r') as f:
+        params = json.load(f)
+    return params
+
+def get_scaled_score(unscaled_score, params):
+    """
+    unscaled_score: QScore integer value without any scale applied to it
+    params: dictionary containing relevant parameters for scale for that task
+
+    Returns: scaled_score: integer representing QScore, with range of 1-100
+    """
+    scaled_score = ((unscaled_score - params['data_min']) / (params['data_max'] - params['data_min'])) * 100
+    scaled_score = int(np.clip(scaled_score, 1, 100))
+    return scaled_score
 
 def post_score(task_name: str, metric_name: str, result: int):
     """
